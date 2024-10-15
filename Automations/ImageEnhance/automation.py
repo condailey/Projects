0c3815b8ebb76cf7213d1dc3cf5e7a569e9e9b47
condailey <condailey1@gmail.com>
2024-10-15 15:46:05 -0500

    Create automation.py

---
+++ Automations/ImageEnhance/automation.py
@@ -0,0 +1,21 @@
+#Inspired by a tutorial. Taught me how to use PIL in enhancing images, reading from a file, and referencing paths to files on my computer.#
+
+from PIL import Image, ImageEnhance, ImageFilter
+import os
+
+path = './imgs'
+pathOut = '/editedImgs'
+
+for filename in os.listdir(path):
+    img = Image.open(f"{path}/{filename}")
+
+
+    edit = img.filter(ImageFilter.SHARPEN).convert('L')
+
+    factor = 1.5
+    enhancer = ImageEnhance.Contrast(edit)
+    edit = enhancer.enhance(factor)
+
+    clean_name = os.path.splitext(filename)[0]
+
+    edit.save(f'.{pathOut}/{clean_name}_edited.jpg')
